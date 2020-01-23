d585168364a49252eb0295aa04644b1f70fc3b21
Гузель Шарафутдинова <guzelsarafutdinova@MacBook-Pro-Guzel.local>
2020-01-23 18:39:29 +0300

    First commit

---
+++ CICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICD
+//
+//  Created by Гузель Шарафутдинова on 23/01/2020.
+//  Copyright © 2020 Гузель Шарафутдинова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
